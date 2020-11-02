935d0653bf5f8049fb8cf5f34d3218d6f018f8b7
Gift-Tanakan <50960357+Gift-Tanakan@users.noreply.github.com>
2020-11-01 18:02:48 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Gift's Portfolio</title>
+        <link href="css/styles.css" type="text/css" rel="stylesheet">
+    </head>
+
+    <body>
+        <header>
+            <div class="row">
+                <a href="index.php"><img id="logo" src="images/portfolio_logo_white_new.png" alt="Portfolio logo"></a>
+                <div class="headtext">
+                    <h1>Gift - Tanakan Homsaen</h1>
+                    <h2 class="h2-head">Web Developer | Work &amp; Experience</h2>
+                </div>
+            </div>
+        </header>
+        <nav>
+            <!--        <div class="row">-->
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="">Work</a></li>
+                <li><a href="https://github.com/Gift-Tanakan" target="_blank">GitHub</a></li>
+                <li><a href="">Testimonials</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+            <!--        </div>end row-->
+        </nav>
+        <div class="wrapper">
+            <main>
+                <h2 class="welcome">Welcome to my portfolio!</h2>
+                <img class= "profile_img" src ="images/GiftRedimension.png" alt="GiftImage">
+                <p>My name is Gift. I'm studying Programming Associate Degree at Seattle Central College. I work part-time as a web manager at a student-run journal - the Seattle Collegian. Despite uncertenties of everything we're facing right now, I still have a dream. I want to master my skills in web development and programming and utilize them in the causes I care about most, the environment and climate change. The organizations that inspire me to be a part of are Ecosia, TerraCycle, and Too Good To Go. Outside of studying and working, I enjoy baking, painting, reading articles. I find Harvard Business Review's articles fascinating, especially the self-development category. Those stories help broaden my perspectives personally and professionally. They are so clever at curating graphics for each article. Nail it every time!
+                </p>
+                
+                <h2>What I do?</h2>
+                <ul>
+                    <li>Wireframing</li>
+                    <li>Redesigning websites</li>
+                    <li>Visualizing your purposes</li>
+                    
+                </ul>
+            </main>
+
+
+            <aside>
+                <h2>Work</h2>
+                <ul>
+                    <li><a href="https://www.scoopmarketplace.com/" target="_blank">Scoop Marketplace</a></li>
+                    <li><a href="https://www.shopscooponline.com/" target="_blank">Scoop Marketplace Online Shop</a></li>
+                    <li><a href="index.php">HW2: Portal Page</a></li>
+                    <li>Week 2: Class Exercises:(<a href="week2/var.php">var</a>/<a href="week2/heredoc.php">heredoc</a>/<a href="week2/currency.php">currency</a>/<a href="week2/currency-logic.php">currency-logic)</a></li>
+                    <li><a href="daily.php">HW3: Using a Switch</a></li>
+                    <li>Week 3: Class Exercises:(<a href="week3/arrays.php">arrays</a>/<a href="week3/html-page.php">html-page</a>/<a href="week3/if.php">if</a>/<a href="week3/switch.php">switch</a>/<a href="site1/daily.php">daily</a>)</li>
+                
+
+                </ul>
+            </aside>
+        </div> <!--end wrapper-->
+
+        <footer>
+            <div class="copyright">
+                <div class="row">
+
+                    <ul>
+                        <li>Copyright 2019 - <?php
+                            echo date('Y');
+                            ?>
+                        </li>
+                        <li>All Rights Reserved</li>
+                        
+                        <li><a href="index.php">Web Design by Gift</a></li>
+
+                    </ul>
+                </div><!--end row-->
+            </div> <!--end copyright-->
+        </footer>
+
+    </body>
+</html> 
