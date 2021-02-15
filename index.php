f91908eddf5db8eee4eb5fdfda87b4c63d541274
Gift-Tanakan <50960357+Gift-Tanakan@users.noreply.github.com>
2021-02-15 11:45:04 -0800

    include header, footer

--- index.php
+++ index.php
@@ -1,44 +1,16 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width" />
-        <title>Gift's Portfolio</title>
-        <link href="css/styles.css" type="text/css" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<?php  
 
-    </head>
+include('config.php'); 
+include('includes/header.php');
 
-    <body>
-        <div class="under-header">
-            </div>
-        <div class="wrapper">
-            <header>
-                <a href="index.php"><img id="logo" src="images/portfolio_logo_white_new.png" alt="Portfolio logo"></a>
-                <div class="headtext">
-                    <h1>Gift - Tanakan Homsaen</h1>
-                    <h2 class="h2-head">Web Developer | Work &amp; Experience</h2>
-                </div>
-            </header>
-                
-            <nav class="topnav" id="myTopnav">
-                
-                <a href="index.php" class="active">Home</a>
-                <a href="index.php">Work</a>
-                <a href="index.php">Testimonials</a>
-                <a href="index.php">About</a>
-                <a href="index.php">Contact</a>
-                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                    <i class="fa fa-bars"></i>
-                </a>  
-            </nav>
+?>
             <main>
                 <div id="short-bio">
                 <h2 class="welcome">Welcome to my portfolio!</h2>
                     <div class="profile_img">
                         <img src ="images/GiftRedimension.png" alt="GiftImage"> </div>
 <!--                <p>My name is Gift. I'm studying Programming Associate Degree at Seattle Central College. I also work as a web manager at a student-run newspaper - The Seattle Collegian. Despite uncertainties of everything we're facing right now, I still have a dream. I want to master my skills in web development and programming and utilize them in the causes I care about most, the environment and climate change.</p>-->
-                <p>My name is Gift, a programming student at Seattle Central College and a web developer at The Seattle Collegian, a student-run newspaper. I also build and redesign websites for local small businesses. Starting from clarifying the purposes of their online presence, values, and unique selling points, curating ideas and communication methods, structuring the layout, and building the website.</p>
+                <p>My name is Gift, a Programming student at Seattle Central College and a web developer at The Seattle Collegian, a student-run newspaper. I also build and redesign websites for local small businesses. Starting from clarifying the purposes of their online presence, values, and unique selling points, curating ideas and communication methods, structuring the layout, and building the website.</p>
                 
 <!--
                 I'd be more than happy to work with you whether you want to redesign the existing website or build a new one. Explore my work and and contact me.  
@@ -54,7 +26,7 @@
                             <img src ="images/scoop_home.png" alt="Scoop Marketplace Home Page">
                         </div>
                             <div class="work_points">
-                                <h3>Project1</h3>
+                                <h3><a href="#">Project1</a></h3>
                                 <ul>
                                     <li>work1
                     My name is Gift, a programming  </li>
@@ -72,7 +44,7 @@
                             <img src ="images/scoop_online_home.png" alt="Scoop Marketplace Home Page">
                         </div>
                             <div class="work_points">
-                                <h3>Project2</h3>
+                                <h3><a href="#">Project2</a></h3>
                                 <ul>
                                     <li>work1
                     My name is Gift, a programming  </li>
@@ -91,7 +63,7 @@
                             <img src ="images/scoop_home.png" alt="Scoop Marketplace Home Page">
                         </div>
                             <div class="work_points">
-                                <h3>Project1</h3>
+                                <h3><a href="#">Project3</a></h3>
                                 <ul>
                                     <li>work1
                     My name is Gift, a programming  </li>
@@ -109,7 +81,7 @@
                             <img src ="images/scoop_online_home.png" alt="Scoop Marketplace Home Page">
                         </div>
                             <div class="work_points">
-                                <h3>Project2</h3>
+                                <h3><a href="#">Project4</a></h3>
                                 <ul>
                                     <li>work1
                     My name is Gift, a programming  </li>
@@ -122,7 +94,58 @@
                     </div>
                     </div> <!--end item-->
                     
+                    
                     </div> <!---end container-->
+                    
+                    <button type="button" class="button"><a href="#">Browse More</a></button>
+                    
+            </div> <!--End work section--->
+                
+                <div id="testimonials">
+                <h2>Testimonials</h2>
+                    
+                <div class="testimonials">
+                    <div class="test">
+
+                            <div>
+
+                                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                                </p>
+                                <p>"qui officia deserunt mollit anim id est laborum."</p>
+                    </div>
+                    </div> <!--end item-->
+                    
+                    <div class="test">
+
+                            <div>
+                                
+                                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                                </p>
+                                <p>"qui officia deserunt mollit anim id est laborum."</p>
+                    </div>
+                    </div> <!--end item-->
+                    
+                    <div class="test">
+
+                            <div>
+                                
+                                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                                </p>
+                                <p>"qui officia deserunt mollit anim id est laborum."</p>
+                    </div>
+                    </div> <!--end item-->
+                    
+                    <div class="test">
+
+                            <div>
+                                
+                                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                                </p>
+                                <p>"qui officia deserunt mollit anim id est laborum."</p>
+                    </div>
+                    </div> <!--end item-->
+                    
+                    </div> <!---end testimonials-->
                     
                     
                     
@@ -161,42 +184,7 @@
                 </ul>
 -->
             </aside>
-<!--
-            <div class="under-footer">
-            </div>
--->
-            <footer>
-                
-            <div class="copyright">
-                <p>Copyright 2019 - <?php
-                            echo date('Y');
-                            ?> | All Rights Reserved | <a href="index.php">Web Design by Gift</a></p>
-                    
-                    <ul>
-                        <li>Copyright 2019 - <?php
-                            echo date('Y');
-                            ?>
-                        </li>
-                        <li>All Rights Reserved</li>
-                        
-                        <li><a href="index.php">Web Design by Gift</a></li>
-                    </ul>
-
-            </div> <!--end copyright-->
-            </footer>
+<?php 
+    include('includes/footer.php');
+    ?>
             
-        </div> <!--End Wrapper--->
-        
-        <script>
-            function myFunction() {
-                var x = document.getElementById("myTopnav");
-                if (x.className === "topnav") {
-                    x.className += " responsive";
-                } else {
-                    x.className = "topnav";
-                }
-            }
-        </script>
-
-    </body>
-</html>     
