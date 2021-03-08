6a130b07b7efda98c1615da73d1fb3af59c919ee
Gift-Tanakan <50960357+Gift-Tanakan@users.noreply.github.com>
2021-03-08 11:49:05 -0800

    Update zerowastenav.php

--- zerowastenav.php
+++ zerowastenav.php
@@ -4,59 +4,38 @@ include('config.php');
 include('includes/header.php');
 
 ?>
-<main>
+<main id="projects">
+    <div>
+        <h2 class="welcome">Zero Waste Navigator Introduction</h2>
+    </div> <!--End work section--->
     
-<!----------    tempo nav links delete when have proper nav------------->
-    
-<div class="cat-nav">
-<!--
-        <a href="#">Wireframing</a>
-        <a href="#">Web Designing</a>
-        <a href="#">Web Editing</a>
--->
-        <p>Go to ></p>
-        <a href="scoopwix.php">Scoop on Wix</a>
-        <a href="scoopsquare.php">Scoop on Square</a>
-        <a href="zerowastenav.php">School Project: Zero-waste nav app</a>
-        <a href="mindfulgiftguide.php">School Project: Mindful gift guide</a>
-
-    </div>
-    
-    <!----------    end draft nav links------------->
+<!------------breadcrumb----------->
+<ul class="breadcrumb">
+  <li><a href="projects.php" id="project_link">Projects</a></li>
+  <li><a href="zerowastenav.php" id="project_link">Zero Waste Navigator Introduction</a></li>
+</ul>
+    <!------------ end breadcrumb----------->
     
     <div class="testimonials">
         <div class="test">
-
-            <h3>An App Introduction Website</h3>
 
             <div class="work_img">
                 <img class="border" src ="images/zero_home_top.png" alt="Zero-Waste Navigator App Intro Homepage">
             </div>
 
             <div class="work_points">
-                <h3>School Project: Zero-Waste Navigator App Intro Website</h3>
-                <ul>
-                    <li><strong>Store overview</strong>: Scoop Marketplace is a zero-waste grocery store orginally located in Seattle, now in Kirkland. The store lets customers bring their reusable containers to scoop package-free food, home goods, and personal care products in their desire amount.</li>
-                    <li><strong>Brief story</strong>: I was a founding member of Scoop Marketplace with a strong interest in helping </li>
-                    <li><strong>Structure</strong>: HTML, CSS</li>
-                    <li><strong>View Website</strong>: <a href="https://www.gifttanakan.com/final-project-special-edition/index.html" target="_blank">Zero Waste Navigator Application</a></li>
-                    <li><strong>Accomplishments</strong>: 
-                        <ul>
-                            <li>Website development (entire site)</li>
-                            <li>Responsive design (mobile)</li>
-                        </ul>
-                    
-                    <li><strong>Note</strong>: Now the business has had a website makeover with another e-commerce platform.</li>
-
-                </ul>
-            </div>
+                <h3>School Project: Zero-Waste Navigator App Intro Website Homepage</h3>
+                
+            </div> <!--end work_points-->
+            
+            
 
             <div class="work_img">
                 <img class="border" src ="images/zero_home_value.png" alt="Zero-Waste Navigator App Intro Website">
             </div>
 
             <div class="work_points">
-                <h3>Set up layout for online shopping page </h3>
+                <h3>Homepage: describing the users' benefits and impacts they can make</h3>
             </div>
 
             <div class="work_img">
@@ -64,11 +43,7 @@ include('includes/header.php');
             </div>
 
             <div class="work_points">
-                <h3>In-store products category page (Scroll to view the whole page)</h3>
-                <ul>
-                <li><strong>Challenge</strong>: In-store bulk products availability couldn't be tracked precisely through Square's POS system. Therefore, I didn't pull categories information from the system to create the category page.</li>
-                <li><strong>Solution</strong>: I created the page manually from a custom page by using a template, adjusting content, and adding attribute accordingly.</li>
-                </ul>
+                <h3>Homepage: testimonials from the users to build the app's credibility</h3>
 
             </div>
 
@@ -77,38 +52,16 @@ include('includes/header.php');
             </div>
 
             <div class="work_points">
-                <h3>In-store products list pages (Scroll to view the whole page)</h3>
-                <ul>
-                <li><strong>Challenge</strong>: This template doesn't have any side navigation, which is not helpful for users who want to explore other categories. The business may lose opportunity to keep users engaged.</li>
-                <li><strong>Solution</strong>: At the bottom of every category page, I added a custom HTML and CSS links to show other pages (other categories) that they can visit.</li>
-                </ul>
+                <h3>Store Partners: describing the partners of the app</h3>
 
             </div>
             
             <div class="work_img">
                 <img class="border" src ="images/zero_about.png" alt="Zero-Waste Navigator App About Page">
             </div>
-
-            <div class="work_points">
-                <h3>In-store products list pages (Scroll to view the whole page)</h3>
-                <ul>
-                <li><strong>Challenge</strong>: This template doesn't have any side navigation, which is not helpful for users who want to explore other categories. The business may lose opportunity to keep users engaged.</li>
-                <li><strong>Solution</strong>: At the bottom of every category page, I added a custom HTML and CSS links to show other pages (other categories) that they can visit.</li>
-                </ul>
-
-            </div>
             
             <div class="work_img">
                 <img class="border" src ="images/zero_about_content.png" alt="Zero-Waste Navigator App About Page">
-            </div>
-
-            <div class="work_points">
-                <h3>In-store products list pages (Scroll to view the whole page)</h3>
-                <ul>
-                <li><strong>Challenge</strong>: This template doesn't have any side navigation, which is not helpful for users who want to explore other categories. The business may lose opportunity to keep users engaged.</li>
-                <li><strong>Solution</strong>: At the bottom of every category page, I added a custom HTML and CSS links to show other pages (other categories) that they can visit.</li>
-                </ul>
-
             </div>
             
             <div class="work_img">
@@ -116,55 +69,40 @@ include('includes/header.php');
             </div>
 
             <div class="work_points">
-                <h3>In-store products list pages (Scroll to view the whole page)</h3>
-                <ul>
-                <li><strong>Challenge</strong>: This template doesn't have any side navigation, which is not helpful for users who want to explore other categories. The business may lose opportunity to keep users engaged.</li>
-                <li><strong>Solution</strong>: At the bottom of every category page, I added a custom HTML and CSS links to show other pages (other categories) that they can visit.</li>
+                <h3>About Us: describing and showing how the app works in motion</h3>
+            
+            <ul>
+                    <li><strong>Project overview</strong>: A final project of Web Authoring class - an application introduction website aiming to encourage users to download it. The application is called Zero-Waste Navigator. It's a tool that helps users find package-free, bulk products nearest to their location, where they can bring reusable containers to shop.</li>
+                    <li><strong>Technology</strong>: HTML, CSS</li>
+                    <li><strong>Year</strong>: 2020</li>
+                    <li><strong>View Website</strong>: <a href="https://www.gifttanakan.com/final-project-special-edition/index.html" target="_blank" id="link">Zero Waste Navigator Application</a></li>
+                    <li><strong>Note</strong>: This app is in ideation phase.</li>
+                    <li><strong>Accomplishments</strong>: 
+                        <ul>
+                            <li>Wireframing</li>
+                            <li>Website design</li>
+                            <li>Website development</li>
+                        </ul>
+                    </li>
+                    
                 </ul>
-
-            </div>
+                
+                </div>
 
 
         </div> <!--end item-->
 
+        <!------------pagination------------>
 
+<div class="pagination breadcrumb">
+  <a href="artwireframe.php" id="project_link">&#10094; Previous</a>
+  <a href="mindfulgiftguide.php" id="project_link">Next &#10095;</a>
+</div>
+        <!------------end pagination------------>
 
     </div> <!---end testimonials-->
 
 </main>
-<!--   <        aside class="item related-links">Related Links</aside> -->
-<aside>
-    <!--
-<h2>Platforms I use</h2>
-<ul>
-<li>WordPress</li>
-<li>SquareUp</li>
-<li>GoDaddy</li>
-<li>Wix</li>
-</ul>
--->
-    <!--
-<h2>Skills</h2>    
-<ul>
-<li>Web audit</li>
-<li>Research &amp; documentation </li>
-<li>Web content planning</li>
-<li>Layout designing</li>
-<li>Wireframing</li>
-<li>Responsive Design</li>
-<li>HTML, CSS, PHP</li>
-
-</ul>
--->
-    <!--
-<h2>Work</h2>
-<ul>
-<li><a href="https://www.scoopmarketplace.com/" target="_blank">Scoop Marketplace</a></li>
-<li><a href="https://www.shopscooponline.com/" target="_blank">Scoop Marketplace Online Shop</a></li>
-
-</ul>
--->
-</aside>
 
 <?php 
 include('includes/footer.php');
