7c9c7837c50c40121fd152c130d989de6e6e6b80
Gift-Tanakan <50960357+Gift-Tanakan@users.noreply.github.com>
2021-03-08 11:47:41 -0800

    Update scoopsquare.php

--- scoopsquare.php
+++ scoopsquare.php
@@ -4,46 +4,41 @@ include('config.php');
 include('includes/header.php');
 
 ?>
-<main>
-<!----------    tempo nav links delete when have proper nav------------->
+<main id="projects">
+    <div>
+        <h2 class="welcome">Scoop Marketplace Online Shop</h2>
+    </div> <!--End work section--->
     
-<div class="cat-nav">
-<!--
-        <a href="#">Wireframing</a>
-        <a href="#">Web Designing</a>
-        <a href="#">Web Editing</a>
--->
-        <p>Go to ></p>
-        <a href="scoopwix.php">Scoop on Wix</a>
-        <a href="scoopsquare.php">Scoop on Square</a>
-        <a href="zerowastenav.php">School Project: Zero-waste nav app</a>
-        <a href="mindfulgiftguide.php">School Project: Mindful gift guide</a>
-
-    </div>
+<!------------breadcrumb----------->
+    <ul class="breadcrumb">
+        <li><a href="projects.php" id="project_link">Projects</a></li>
+        <li><a href="scoopsquare.php" id="project_link">Scoop Marketplace Online Shop</a></li>
+    </ul>
+<!------------end breadcrumb----------->
     
-    <!----------    end draft nav links------------->
     <div class="testimonials">
         <div class="test">
 
-            <h3>Scoop Marketplace - Zero-waste grocery store</h3>
-
             <div class="work_img">
-                <img src ="images/scoop_online_home_1280x729.png" alt="Scoop Marketplace Online Shop Home Page">
+                <img src ="images/scoop_online_home.png" alt="Scoop Marketplace Online Shop Home Page">
             </div>
 
             <div class="work_points">
-                <h3>Website for a small business on Square</h3>
+                <h3>Scoop Marketplace: E-commerce website for a small business</h3>
                 <ul>
-                    <li><strong>Store overview</strong>: Scoop Marketplace is a zero-waste grocery store orginally located in Seattle, now in Kirkland. The store lets customers bring their reusable containers to scoop package-free food, home goods, and personal care products in their desire amount.</li>
-                    <li><strong>Brief story</strong>: I was a founding member of Scoop Marketplace with a strong interest in helping </li>
-                    <li><strong>Platform</strong>: Square</li>
+                    <li><strong>Store overview</strong>: Scoop Marketplace is a zero-waste grocery store orginally located in Seattle, now in Kirkland. The store lets customers bring their reusable containers to scoop package-free food, home goods, and personal care products in their desire amount. Packaged products are also available in compostable or reusable packaging.</li>
+                    <li><strong>Project overview</strong>: Regarding the <a href="scoopwix.php" target="_blank" id="link">Scoop website on Wix</a>, this e-commerce website was built once the business provided online shopping. The website built on the Point of Sell system, Square, that Scoop was using to provide a better user experience than Wix online store and more robust workflow for the team.</li>
+                    <li><strong>Technology</strong>: Square</li>
+                    <li><strong>Year</strong>: 2019-2020</li>
+                    <li><strong>Note</strong>: This website is no longer live. The business has had a website makeover with another e-commerce platform. Therefore, the domain name is redirected to the new website.</li>
                     <li><strong>Accomplishments</strong>: 
                         <ul>
-                            <li>Website development (entire site)</li>
-                            <li>Responsive design (mobile)</li>
+                            <li>Layout design for online shopping page.</li>
+                            <li>In-store product categories page.</li>
+                            <li>In-store product list pages.</li>
+                            <li>Custom HTML and CSS links that show other categories.</li>
                         </ul>
-                    
-                    <li><strong>Note</strong>: Now the business has had a website makeover with another e-commerce platform.</li>
+                    </li>
 
                 </ul>
             </div>
@@ -53,7 +48,7 @@ include('includes/header.php');
             </div>
 
             <div class="work_points">
-                <h3>Set up layout for online shopping page </h3>
+                <h3>Set up layout for online shopping page</h3>
             </div>
 
             <div class="work_img scroll">
@@ -61,10 +56,10 @@ include('includes/header.php');
             </div>
 
             <div class="work_points">
-                <h3>In-store products category page (Scroll to view the whole page)</h3>
+                <h3>In-store product categories page (Scroll to view the whole page)</h3>
                 <ul>
-                <li><strong>Challenge</strong>: In-store bulk products availability couldn't be tracked precisely through Square's POS system. Therefore, I didn't pull categories information from the system to create the category page.</li>
-                <li><strong>Solution</strong>: I created the page manually from a custom page by using a template, adjusting content, and adding attribute accordingly.</li>
+                <li><strong>Challenge</strong>: In-store bulk products availability couldn't be tracked precisely through Square's POS system. Therefore, I didn't pull categories information from the system to create this page.</li>
+                <li><strong>Solution</strong>: I created the page manually from a custom page by using a template, adjusting content, and adding attributes accordingly.</li>
                 </ul>
 
             </div>
@@ -74,55 +69,28 @@ include('includes/header.php');
             </div>
 
             <div class="work_points">
-                <h3>In-store products list pages (Scroll to view the whole page)</h3>
+                <h3>In-store products list page (Scroll to view the whole page)</h3>
                 <ul>
-                <li><strong>Challenge</strong>: This template doesn't have any side navigation, which is not helpful for users who want to explore other categories. The business may lose opportunity to keep users engaged.</li>
-                <li><strong>Solution</strong>: At the bottom of every category page, I added a custom HTML and CSS links to show other pages (other categories) that they can visit.</li>
+                <li><strong>Challenge</strong>: This template doesn't have any side navigation, which is not helpful for users to browse other categories. The business may lose opportunity to keep user engagement.</li>
+                <li><strong>Solution</strong>: Added custom HTML and CSS links to show other pages (categories) at the bottom of every product list page.</li>
                 </ul>
 
             </div>
 
-
         </div> <!--end item-->
 
+<!------------pagination------------>
 
+<div class="pagination breadcrumb">
+  <a href="scoopwix.php" id="project_link">&#10094; Previous</a>
+  <a href="scoopwireframe.php" id="project_link">Next &#10095;</a>
+</div>
+        <!------------end pagination------------>
 
     </div> <!---end testimonials-->
 
 </main>
-<!--   <        aside class="item related-links">Related Links</aside> -->
-<aside>
-    <!--
-<h2>Platforms I use</h2>
-<ul>
-<li>WordPress</li>
-<li>SquareUp</li>
-<li>GoDaddy</li>
-<li>Wix</li>
-</ul>
--->
-    <!--
-<h2>Skills</h2>    
-<ul>
-<li>Web audit</li>
-<li>Research &amp; documentation </li>
-<li>Web content planning</li>
-<li>Layout designing</li>
-<li>Wireframing</li>
-<li>Responsive Design</li>
-<li>HTML, CSS, PHP</li>
 
-</ul>
--->
-    <!--
-<h2>Work</h2>
-<ul>
-<li><a href="https://www.scoopmarketplace.com/" target="_blank">Scoop Marketplace</a></li>
-<li><a href="https://www.shopscooponline.com/" target="_blank">Scoop Marketplace Online Shop</a></li>
-
-</ul>
--->
-</aside>
 
 <?php 
 include('includes/footer.php');
