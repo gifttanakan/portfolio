71129907828cbf020d0c4b0f9c60a5305a50df79
Gift-Tanakan <50960357+Gift-Tanakan@users.noreply.github.com>
2021-03-08 11:44:08 -0800

    Add form

--- contact.php
+++ contact.php
@@ -4,186 +4,34 @@ include('config.php');
 include('includes/header.php');
 
 ?>
+
+<?php 
+// notification message
+
+if(isset($_SESSION['success'])) : ?>
+<div class="error success">
+    <h3><?php
+        echo $_SESSION['success'];
+        unset($_SESSION['success']);
+        ?></h3>
+</div>
+
+<?php endif ?>
+
             <main>
                 <div id="short-bio">
-                <h2 class="welcome">Welcome to my portfolio!</h2>
-                    <div class="profile_img">
-                        <img src ="images/GiftRedimension.png" alt="GiftImage"> </div>
-<!--                <p>My name is Gift. I'm studying Programming Associate Degree at Seattle Central College. I also work as a web manager at a student-run newspaper - The Seattle Collegian. Despite uncertainties of everything we're facing right now, I still have a dream. I want to master my skills in web development and programming and utilize them in the causes I care about most, the environment and climate change.</p>-->
-                <p>My name is Gift, a Programming student at Seattle Central College and a web developer at The Seattle Collegian, a student-run newspaper. I also build and redesign websites for local small businesses. Starting from clarifying the purposes of their online presence, values, and unique selling points, curating ideas and communication methods, structuring the layout, and building the website.</p>
-                
-<!--
-                I'd be more than happy to work with you whether you want to redesign the existing website or build a new one. Explore my work and and contact me.  
-                </p>
--->
+                <h2 class="welcome">Contact</h2>
+
+                    <p>Send your questions or feedback about my services, past projects, or report bug through this form. I'll also be happy to connect with you on <a href="https://twitter.com/Gift_Tanakan" target="_blank" id="link">Twitter</a>, <a href="https://github.com/Gift-Tanakan" target="_blank" id="link">GitHub</a>, <a href="https://www.linkedin.com/in/gift-tanakan-homsaen-082210120/" target="_blank" id="link">LinkedIn</a>. And if you like this portfolio, have a read about how I craft it <a href="https://community.codenewbie.org/gift_tanakan/in-progress-portfolio-current-state-and-why-i-build-this-thing-from-scratch-22f2" target="_blank" id="link">here</a>.</p>
                 </div>
-                <div id="work-section">
-                <h2>Work &amp; Experience</h2>
+                <div id="about-bio">
+                    <?php include('includes/form.php'); ?>
                     
-                <div class="container">
-                    <div class="item project1">
-                        <div class="work_img">
-                            <img src ="images/scoop_home.png" alt="Scoop Marketplace Home Page">
-                        </div>
-                            <div class="work_points">
-                                <h3><a href="#">Project1</a></h3>
-                                <ul>
-                                    <li>work1
-                    My name is Gift, a programming  </li>
-                                    <li>work1
-                    My name is Gift, a programming </li>
-                                    <li>work1
-                    My name is Gift, a programming </li>
-                                
-                                </ul>
-                    </div>
-                    </div> <!--end item-->
-                    
-                    <div class="item project2">
-                        <div class="work_img">
-                            <img src ="images/scoop_online_home.png" alt="Scoop Marketplace Home Page">
-                        </div>
-                            <div class="work_points">
-                                <h3><a href="#">Project2</a></h3>
-                                <ul>
-                                    <li>work1
-                    My name is Gift, a programming  </li>
-                                    <li>work1
-                    My name is Gift, a programming </li>
-                                    <li>work1
-                    My name is Gift, a programming </li>
-                                
-                                </ul>
-                    </div>
-                    </div> <!--end item-->
-                    
-                    
-                    <div class="item project3">
-                        <div class="work_img">
-                            <img src ="images/scoop_home.png" alt="Scoop Marketplace Home Page">
-                        </div>
-                            <div class="work_points">
-                                <h3><a href="#">Project3</a></h3>
-                                <ul>
-                                    <li>work1
-                    My name is Gift, a programming  </li>
-                                    <li>work1
-                    My name is Gift, a programming </li>
-                                    <li>work1
-                    My name is Gift, a programming </li>
-                                
-                                </ul>
-                    </div>
-                    </div> <!--end item-->
-                    
-                    <div class="item project4">
-                        <div class="work_img">
-                            <img src ="images/scoop_online_home.png" alt="Scoop Marketplace Home Page">
-                        </div>
-                            <div class="work_points">
-                                <h3><a href="#">Project4</a></h3>
-                                <ul>
-                                    <li>work1
-                    My name is Gift, a programming  </li>
-                                    <li>work1
-                    My name is Gift, a programming </li>
-                                    <li>work1
-                    My name is Gift, a programming </li>
-                                
-                                </ul>
-                    </div>
-                    </div> <!--end item-->
-                    
-                    
-                    </div> <!---end container-->
-                    
-                    <button type="button" class="button"><a href="#">Browse More</a></button>
-                    
-            </div> <!--End work section--->
+                </div>
                 
-                <div id="testimonials">
-                <h2>Testimonials</h2>
-                    
-                <div class="testimonials">
-                    <div class="test">
-
-                            <div>
-
-                                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                                </p>
-                                <p>"qui officia deserunt mollit anim id est laborum."</p>
-                    </div>
-                    </div> <!--end item-->
-                    
-                    <div class="test">
-
-                            <div>
-                                
-                                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                                </p>
-                                <p>"qui officia deserunt mollit anim id est laborum."</p>
-                    </div>
-                    </div> <!--end item-->
-                    
-                    <div class="test">
-
-                            <div>
-                                
-                                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                                </p>
-                                <p>"qui officia deserunt mollit anim id est laborum."</p>
-                    </div>
-                    </div> <!--end item-->
-                    
-                    <div class="test">
-
-                            <div>
-                                
-                                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                                </p>
-                                <p>"qui officia deserunt mollit anim id est laborum."</p>
-                    </div>
-                    </div> <!--end item-->
-                    
-                    </div> <!---end testimonials-->
-                    
-                    
-                    
-            </div>
+                
             </main>
-            <!--   <        aside class="item related-links">Related Links</aside> -->
-            <aside>
-<!--
-            <h2>Platforms I use</h2>
-                <ul>
-                    <li>WordPress</li>
-                    <li>SquareUp</li>
-                    <li>GoDaddy</li>
-                    <li>Wix</li>
-                </ul>
--->
-<!--
-            <h2>Skills</h2>    
-                <ul>
-                    <li>Web audit</li>
-                    <li>Research &amp; documentation </li>
-                    <li>Web content planning</li>
-                    <li>Layout designing</li>
-                    <li>Wireframing</li>
-                    <li>Responsive Design</li>
-                    <li>HTML, CSS, PHP</li>
-                    
-                </ul>
--->
-<!--
-            <h2>Work</h2>
-                <ul>
-                    <li><a href="https://www.scoopmarketplace.com/" target="_blank">Scoop Marketplace</a></li>
-                    <li><a href="https://www.shopscooponline.com/" target="_blank">Scoop Marketplace Online Shop</a></li>
-
-                </ul>
--->
-            </aside>
+           
 <?php 
     include('includes/footer.php');
     ?>
