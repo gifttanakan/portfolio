216f8e2e8adb52c0f88661ecebacb50c4d2f4830
Gift-Tanakan <50960357+Gift-Tanakan@users.noreply.github.com>
2021-03-08 11:46:10 -0800

    Update mindfulgiftguide.php

--- mindfulgiftguide.php
+++ mindfulgiftguide.php
@@ -4,70 +4,31 @@ include('config.php');
 include('includes/header.php');
 
 ?>
-<main>
-<!----------    tempo nav links delete when have proper nav------------->
-    
-<div class="cat-nav">
-<!--
-        <a href="#">Wireframing</a>
-        <a href="#">Web Designing</a>
-        <a href="#">Web Editing</a>
--->
-        <p>Go to ></p>
-        <a href="scoopwix.php">Scoop on Wix</a>
-        <a href="scoopsquare.php">Scoop on Square</a>
-        <a href="zerowastenav.php">School Project: Zero-waste nav app</a>
-        <a href="mindfulgiftguide.php">School Project: Mindful gift guide</a>
+<main id="projects">
+    <div>
+        <h2 class="welcome">Guide: Mindful gifts for this holiday</h2>
+    </div> <!--End work section--->
 
-    </div>
+    <!------------breadcrumb----------->
+    <ul class="breadcrumb">
+        <li><a href="projects.php" id="project_link">Projects</a></li>
+        <li><a href="mindfulgiftguide.php" id="project_link">Guide: Mindful gifts for this holiday</a></li>
+    </ul>
+<!------------end breadcrumb----------->
     
-    <!----------    end draft nav links------------->
     <div class="testimonials">
         <div class="test">
-
-            <h3>A Mindful Gift Guide Website</h3>
 
             <div class="work_img">
                 <img class="border" src ="images/gift_home.png" alt="A Mindful Gift Guide Homepage">
             </div>
 
             <div class="work_points">
-                <h3>School Project: Guide: Mindful gifts for this holiday</h3>
-                <ul>
-                    <li><strong>Store overview</strong>: Scoop Marketplace is a zero-waste grocery store orginally located in Seattle, now in Kirkland. The store lets customers bring their reusable containers to scoop package-free food, home goods, and personal care products in their desire amount.</li>
-                    <li><strong>Brief story</strong>: I was a founding member of Scoop Marketplace with a strong interest in helping </li>
-                    <li><strong>Structure</strong>: HTML, CSS, PHP</li>
-                    <li><strong>View Website</strong>: <a href="https://www.gifttanakan.com/it261/final-project/index.php" target="_blank">Guide: Mindful gifts for this holiday</a></li>
-                    <li><strong>Accomplishments</strong>: 
-                        <ul>
-                            <li>Website development (entire site)</li>
-                            <li>Responsive design (mobile)</li>
-                        </ul>
-                    
-                    <li><strong>Note</strong>: Now the business has had a website makeover with another e-commerce platform.</li>
-
-                </ul>
-            </div>
-
-            <div class="work_img">
-                <img class="border" src ="images/gift_home_footer.png" alt="A Mindful Gift Guide Homepage Footer">
-            </div>
-
-            <div class="work_points">
-                <h3>Set up layout for online shopping page </h3>
+                <h3>School Project: Guide: Mindful gifts for this holiday homepage</h3>
             </div>
 
             <div class="work_img">
                 <img class="border" src ="images/gift_why.png" alt="A Mindful Gift Guide Why Page">
-            </div>
-
-            <div class="work_points">
-                <h3>In-store products category page (Scroll to view the whole page)</h3>
-                <ul>
-                <li><strong>Challenge</strong>: In-store bulk products availability couldn't be tracked precisely through Square's POS system. Therefore, I didn't pull categories information from the system to create the category page.</li>
-                <li><strong>Solution</strong>: I created the page manually from a custom page by using a template, adjusting content, and adding attribute accordingly.</li>
-                </ul>
-
             </div>
 
             <div class="work_img">
@@ -75,11 +36,7 @@ include('includes/header.php');
             </div>
 
             <div class="work_points">
-                <h3>In-store products list pages (Scroll to view the whole page)</h3>
-                <ul>
-                <li><strong>Challenge</strong>: This template doesn't have any side navigation, which is not helpful for users who want to explore other categories. The business may lose opportunity to keep users engaged.</li>
-                <li><strong>Solution</strong>: At the bottom of every category page, I added a custom HTML and CSS links to show other pages (other categories) that they can visit.</li>
-                </ul>
+                <h3>Why? page: describing reasons to gift mindfully</h3>
 
             </div>
             
@@ -88,11 +45,7 @@ include('includes/header.php');
             </div>
 
             <div class="work_points">
-                <h3>In-store products list pages (Scroll to view the whole page)</h3>
-                <ul>
-                <li><strong>Challenge</strong>: This template doesn't have any side navigation, which is not helpful for users who want to explore other categories. The business may lose opportunity to keep users engaged.</li>
-                <li><strong>Solution</strong>: At the bottom of every category page, I added a custom HTML and CSS links to show other pages (other categories) that they can visit.</li>
-                </ul>
+                <h3>Gifts page: overview about what to give</h3>
 
             </div>
             
@@ -101,11 +54,7 @@ include('includes/header.php');
             </div>
 
             <div class="work_points">
-                <h3>In-store products list pages (Scroll to view the whole page)</h3>
-                <ul>
-                <li><strong>Challenge</strong>: This template doesn't have any side navigation, which is not helpful for users who want to explore other categories. The business may lose opportunity to keep users engaged.</li>
-                <li><strong>Solution</strong>: At the bottom of every category page, I added a custom HTML and CSS links to show other pages (other categories) that they can visit.</li>
-                </ul>
+                <h3>Gifts page: describing ways to give</h3>
 
             </div>
             
@@ -114,11 +63,7 @@ include('includes/header.php');
             </div>
 
             <div class="work_points">
-                <h3>In-store products list pages (Scroll to view the whole page)</h3>
-                <ul>
-                <li><strong>Challenge</strong>: This template doesn't have any side navigation, which is not helpful for users who want to explore other categories. The business may lose opportunity to keep users engaged.</li>
-                <li><strong>Solution</strong>: At the bottom of every category page, I added a custom HTML and CSS links to show other pages (other categories) that they can visit.</li>
-                </ul>
+                <h3>Connect page: stay connect with users through form</h3>
 
             </div>
             
@@ -127,10 +72,21 @@ include('includes/header.php');
             </div>
 
             <div class="work_points">
-                <h3>In-store products list pages (Scroll to view the whole page)</h3>
+                <h3>Connect page: form shows errors on unfilled areas</h3>
                 <ul>
-                <li><strong>Challenge</strong>: This template doesn't have any side navigation, which is not helpful for users who want to explore other categories. The business may lose opportunity to keep users engaged.</li>
-                <li><strong>Solution</strong>: At the bottom of every category page, I added a custom HTML and CSS links to show other pages (other categories) that they can visit.</li>
+                    <li><strong>Project overview</strong>: A final project of Web Application class, focusing on developing and designing a dynamic website. With the concept of sustainability, it aims to emphasize the reasons of mindful gift giving, suggest the ways to do so, and connect with users through a form.</li>
+                    <li><strong>Technology</strong>: HTML, CSS, PHP, SQL</li>
+                    <li><strong>Year</strong>: 2020 - 2021</li>
+                    <li><strong>View Website</strong>: <a href="https://www.gifttanakan.com/it261/final-project/index.php" target="_blank" id="link">Guide: Mindful gifts for this holiday</a></li>
+                    <li><strong>Accomplishments</strong>: 
+                        <ul>
+                            <li>Website design and development</li>
+                            <li>User account creation: register, log in, log out</li>
+                            <li>Contact form: gather users' interest and contact information.</li>
+                            <li>Database integration</li>
+                        </ul>
+                    </li>
+                    
                 </ul>
 
             </div>
@@ -138,44 +94,17 @@ include('includes/header.php');
 
         </div> <!--end item-->
 
+<!------------pagination------------>
 
+<div class="pagination breadcrumb">
+  <a href="zerowastenav.php" id="project_link">&#10094; Previous</a>
+  <a href="diversityofart.php" id="project_link">Next &#10095;</a>
+</div>
+        <!------------end pagination------------>
 
     </div> <!---end testimonials-->
 
 </main>
-<!--   <        aside class="item related-links">Related Links</aside> -->
-<aside>
-    <!--
-<h2>Platforms I use</h2>
-<ul>
-<li>WordPress</li>
-<li>SquareUp</li>
-<li>GoDaddy</li>
-<li>Wix</li>
-</ul>
--->
-    <!--
-<h2>Skills</h2>    
-<ul>
-<li>Web audit</li>
-<li>Research &amp; documentation </li>
-<li>Web content planning</li>
-<li>Layout designing</li>
-<li>Wireframing</li>
-<li>Responsive Design</li>
-<li>HTML, CSS, PHP</li>
-
-</ul>
--->
-    <!--
-<h2>Work</h2>
-<ul>
-<li><a href="https://www.scoopmarketplace.com/" target="_blank">Scoop Marketplace</a></li>
-<li><a href="https://www.shopscooponline.com/" target="_blank">Scoop Marketplace Online Shop</a></li>
-
-</ul>
--->
-</aside>
 
 <?php 
 include('includes/footer.php');
